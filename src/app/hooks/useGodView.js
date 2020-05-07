67fad2655971aadd570d9e0f31e5f27cb551bbc1
Abdelbasite Badou <abdelbassite.badou@gmail.com>
2020-05-07 17:36:18 +0000

    cach godViewData 1 day ahead

--- src/app/hooks/useGodView.js
+++ src/app/hooks/useGodView.js
@@ -14,10 +14,37 @@ const QUERY = gql`
 	}
 `;
 
+// return the date 1 day in the past
+function toPreviousDate(date) {
+	let dateISOString = date.split("/").reverse().join("-");
+	let dateMs = new Date(dateISOString).getTime();
+	dateMs -= 1000 * 60 * 60 * 24;
+	return new Date(dateMs)
+		.toISOString()
+		.slice(0, 10)
+		.split("-")
+		.reverse()
+		.join("/");
+}
+// dates in the array are the date who's previous day's data is already fetched
+// the first day of the time series is a special cases, since it has no previous day
+var fetchedDates = ["01/01/2020"];
 const useGodView = (date) => {
-	const { loading, error, data } = useQuery(QUERY, {
+	const { loading, error, data, client } = useQuery(QUERY, {
 		variables: { date },
 	});
+	let dateIsFetched = fetchedDates.find((elem) => elem === date);
+	// fetch the previous day's data if the current day's data if retreived for the first time
+	if (!dateIsFetched) {
+		fetchedDates.push(date);
+		let previousDate = toPreviousDate(date);
+		console.log("previousDate", previousDate);
+		client.query({
+			query: QUERY,
+			variables: { date: previousDate },
+		});
+	}
+
 	return { loading, error, godViewData: data };
 };
 
