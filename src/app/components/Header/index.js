274b8f58ee6a8d2d27ec585cfd5ab967289e079f
Abdelbasite Badou <abdelbassite.badou@gmail.com>
2020-05-19 23:46:22 +0000

    Header & Footer tweaks

--- src/app/components/Header/index.js
+++ src/app/components/Header/index.js
@@ -1,9 +1,15 @@
 import React from "react";
+import "./styles.sass";
 
 const Header = () => {
 	return (
 		<div id="header">
 			<h1>Covid-19 Dashboard</h1>
+			<div>
+				<span>About</span>
+				<span>Contact</span>
+				<span>Made by Ba-ddou</span>
+			</div>
 		</div>
 	);
 };
