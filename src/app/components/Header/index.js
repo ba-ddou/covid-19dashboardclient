dc9151ea2039e4622f2dc77abc9cd8a3a1e8f937
Abdelbasite Badou <abdelbassite.badou@gmail.com>
2020-05-23 00:45:24 +0000

    init about & contact antd modals

--- src/app/components/Header/index.js
+++ src/app/components/Header/index.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Instagram } from "react-feather";
+import { Modal } from "antd";
 import "./styles.sass";
 
 const Header = () => {
+	let [aboutModal, setAboutModal] = useState(false);
+	let [contactModal, setContactModal] = useState(false);
 	return (
 		<div id="header">
 			<h1>Covid-19 Dashboard</h1>
 			<div>
-				<span>About</span>
-				<span>Contact</span>
+				<span onClick={(_) => setAboutModal(true)}>About</span>
+				<span onClick={(_) => setContactModal(true)}>Contact</span>
 				<a href="https://www.instagram.com/ba_ddou/" target="_blank">
 					<div className="igButton">
 						<Instagram
@@ -20,6 +23,26 @@ const Header = () => {
 					</div>
 				</a>
 			</div>
+			<Modal
+				title="About"
+				centered
+				footer={false}
+				visible={aboutModal}
+				onCancel={(_) => setAboutModal(false)}>
+				<p>some contents...</p>
+				<p>some contents...</p>
+				<p>some contents...</p>
+			</Modal>
+			<Modal
+				title="Contact"
+				centered
+				footer={false}
+				visible={contactModal}
+				onCancel={(_) => setContactModal(false)}>
+				<p>some contents...</p>
+				<p>some contents...</p>
+				<p>some contents...</p>
+			</Modal>
 		</div>
 	);
 };
