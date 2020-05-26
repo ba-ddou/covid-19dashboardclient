c4afc1c3dea488e356f7ec4175184d5a22d49a00
Abdelbasite Badou <abdelbassite.badou@gmail.com>
2020-05-26 17:05:32 +0000

    pwa promt test

--- src/app/components/Header/index.js
+++ src/app/components/Header/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Instagram } from "react-feather";
 import InfoModals from "app/components/InfoModals";
 import "./styles.sass";
@@ -6,10 +6,35 @@ import "./styles.sass";
 const Header = () => {
 	let [aboutModal, setAboutModal] = useState(false);
 	let [contactModal, setContactModal] = useState(false);
+
+	// let pwaPromptEvent = false;
+	let deferredPrompt = null;
+	useEffect(() => {
+		console.log("root use Effect");
+		window.addEventListener(
+			"beforeinstallprompt",
+			(e) => {
+				console.log("beforeinstallprompt captured");
+				// Prevent the mini-infobar from appearing on mobile
+				e.preventDefault();
+				// Stash the event so it can be triggered later.
+				deferredPrompt = e;
+				// Update UI notify the user they can install the PWA
+			},
+			[]
+		);
+	});
+
 	return (
 		<div id="header">
 			<h1>Covid-19 Dashboard</h1>
 			<div>
+				<span
+					onClick={(_) => {
+						if (deferredPrompt) deferredPrompt.prompt();
+					}}>
+					Install
+				</span>
 				<span onClick={(_) => setAboutModal(true)}>About</span>
 				<span onClick={(_) => setContactModal(true)}>Contact</span>
 				<a href="https://www.instagram.com/ba_ddou/" target="_blank">
--- src/app/index.js
+++ src/app/index.js
@@ -28,27 +28,6 @@ const App = () => {
 
 	let onDateChange = (date) => setDate(date);
 
-	useEffect(() => {
-		console.log("root use Effect");
-		let deferredPrompt;
-		window.addEventListener(
-			"beforeinstallprompt",
-			(e) => {
-				console.log("beforeinstallprompt captured");
-				// Prevent the mini-infobar from appearing on mobile
-				e.preventDefault();
-				// Stash the event so it can be triggered later.
-				deferredPrompt = e;
-				// Update UI notify the user they can install the PWA
-				setTimeout(() => {
-					console.log("install sto");
-					deferredPrompt.prompt();
-				}, 5000);
-			},
-			[]
-		);
-	});
-
 	return (
 		<>
 			<Header />
