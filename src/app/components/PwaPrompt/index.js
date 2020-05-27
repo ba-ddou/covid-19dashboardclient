11c03981ef1e40990f091e2069c1be89fabad15b
Abdelbasite Badou <abdelbassite.badou@gmail.com>
2020-05-27 17:53:16 +0000

    pwa install handler

--- src/app/components/PwaPrompt/index.js
+++ src/app/components/PwaPrompt/index.js
@@ -9,34 +9,36 @@ const PwaPrompt = () => {
 	let deferredPrompt = null;
 	useEffect(() => {
 		console.log("root use Effect");
-		window.addEventListener(
-			"beforeinstallprompt",
-			(e) => {
-				console.log("beforeinstallprompt captured");
-				// Prevent the mini-infobar from appearing on mobile
-				e.preventDefault();
-				// Stash the event so it can be triggered later.
-				deferredPrompt = e;
-				// Update UI notify the user they can install the PWA
-				setTimeout(() => {
+		window.addEventListener("beforeinstallprompt", (e) => {
+			console.log("beforeinstallprompt captured");
+			// Prevent the mini-infobar from appearing on mobile
+			e.preventDefault();
+			// Stash the event so it can be triggered later.
+			deferredPrompt = e;
+			console.log("PwaPrompt -> deferredPrompt", deferredPrompt);
+			// Update UI notify the user they can install the PWA
+			setTimeout(() => {
 				setPromptVisible(true);
-				}, 200)
-			});
-			
-			
-	},[]);
+			}, 200);
+		});
+	}, []);
 
-	let install = (_) => {
-		console.log("user wants to install the app");
-		if (deferredPrompt) deferredPrompt.prompt();
-		else console.log("deferredPrompt is null");
-	};
+	// let install = (_) => {
+	// 	console.log("user wants to install the app");
+	// 	if (deferredPrompt) deferredPrompt.prompt();
+	// 	else console.log("deferredPrompt is null");
+	// };
 
 	return (
 		<div>
 			{promptVisible && (
-				<div id="pwaPrompt" >
-					<span onClick={_=>install()}>
+				<div id="pwaPrompt">
+					<span
+						onClick={(_) => {
+							console.log("user wants to install the app");
+							if (deferredPrompt) deferredPrompt.prompt();
+							else console.log("deferredPrompt is null");
+						}}>
 						<Download size="20" />
 						Tap this message to add the app to your homescreen
 					</span>
