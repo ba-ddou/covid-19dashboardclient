79b18ac16c794bd6a259c580805cf5cec23606c0
Abdelbasite Badou <abdelbassite.badou@gmail.com>
2020-05-27 18:26:29 +0000

    pwa promt for ios devices

--- src/app/components/PwaPrompt/index.js
+++ src/app/components/PwaPrompt/index.js
@@ -1,47 +1,57 @@
 import React, { useEffect, useState } from "react";
 import { X, Download } from "react-feather";
+import share from "app/assets/images/share-outline.svg";
 
 import "./styles.sass";
 
 const PwaPrompt = () => {
-	let [promptVisible, setPromptVisible] = useState(false);
+	let [prompt, setPrompt] = useState(false);
 	let [deferredPrompt, setDeferredPrompt] = useState(null);
 	useEffect(() => {
-		console.log("root use Effect");
-		window.addEventListener("beforeinstallprompt", (e) => {
-			console.log("beforeinstallprompt captured");
-			// Prevent the mini-infobar from appearing on mobile
-			e.preventDefault();
-			// Stash the event so it can be triggered later.
-			setDeferredPrompt(e);
-			console.log("PwaPrompt -> deferredPrompt", deferredPrompt);
-			// Update UI notify the user they can install the PWA
-			setTimeout(() => {
-				setPromptVisible(true);
-			}, 200);
-		});
+			console.log("root use Effect");
+			window.addEventListener("beforeinstallprompt", (e) => {
+				console.log("beforeinstallprompt captured");
+				// Prevent the mini-infobar from appearing on mobile
+				e.preventDefault();
+				// Stash the event so it can be triggered later.
+				setDeferredPrompt(e);
+				// Update UI notify the user they can install the PWA
+				setTimeout(() => {
+					setPrompt(
+						<>
+							<Download size="20" />
+							Tap this message to add the app to your homescreen
+						</>
+					);
+				}, 200);
+			});
+		if (['iPhone', 'iPad', 'iPod'].includes(navigator.platform)) {
+			console.log("you're using an apple device")
+		setTimeout(() => {
+			setPrompt(
+				<>
+					Tap <img src={share} /> then 'Add to Home Screen' to install
+					the app
+				</>
+			);
+		}, 200);
+		}
 	}, []);
 
-	// let install = (_) => {
-	// 	console.log("user wants to install the app");
-	// 	if (deferredPrompt) deferredPrompt.prompt();
-	// 	else console.log("deferredPrompt is null");
-	// };
+	let install = (_) => {
+		console.log("user wants to install the app");
+		if (deferredPrompt) {
+			deferredPrompt.prompt();
+			setPrompt(false);
+		} else console.log("deferredPrompt is null");
+	};
 
 	return (
 		<div>
-			{promptVisible && (
+			{prompt && (
 				<div id="pwaPrompt">
-					<span
-						onClick={(_) => {
-							console.log("user wants to install the app");
-							if (deferredPrompt) deferredPrompt.prompt();
-							else console.log("deferredPrompt is null");
-						}}>
-						<Download size="20" />
-						Tap this message to add the app to your homescreen
-					</span>
-					<X size="14" onClick={(_) => setPromptVisible(false)} />
+					<span onClick={(_) => install()}>{prompt}</span>
+					<X size="14" onClick={(_) => setPrompt(false)} />
 				</div>
 			)}
 		</div>
